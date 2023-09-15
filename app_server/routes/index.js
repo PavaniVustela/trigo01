406d06de3576f11a2d4fc302d15562aa313b0ef9
104 <snehalboyinapally24@gmail.com>
2023-09-15 14:37:08 +0530

    first commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const ctrlLocations =
+  require('../controllers/locations');
+const ctrlothers = require('../controllers/others');
+/* GET home page. */
+router.get('/',ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationinfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+router.get('/about',ctrlothers.about);
+module.exports = router;
